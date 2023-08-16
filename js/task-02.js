a135bfb4bff4b21ebc95f234a87cc4924beea540
BalabaSerhii <balabaserhii@gmail.com>
2023-08-16 20:47:48 +0200

    пр

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,29 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredientsEl = document.querySelector("#ingredients");
+
+
+
+   
+   const btnAddd = document.querySelector('.btnAdd')
+const btnRem = document.querySelector('.btnRem')
+let isListAdded = false;
+
+btnAddd.addEventListener('click', () => {
+  const ddd = ingredients.forEach(ingredient => {
+  const test = document.createElement('li');
+test.textContent = `${ingredient}`;
+ingredientsEl.append(test)
+})
+})
+
+
+
+btnRem.addEventListener('click', () => {
+
+  ingredientsEl.innerHTML = '';
+  
+})
